d01641444436ba46e9bcbaf8c33240aff5a857a2
SargamG <166918658+SargamG@users.noreply.github.com>
2026-01-01 16:40:40 +0530

    Update scoring_script.py

--- scoring_script.py
+++ scoring_script.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sys
+import json
 from sklearn.metrics import f1_score, roc_auc_score
 
 """
@@ -74,6 +75,14 @@ def main():
     print(f"Node Macro-F1 : {node_f1:.4f}")
     print(f"Link ROC-AUC  : {link_auc:.4f}")
     print(f"Final Score  : {final_score:.4f}")
+    scores = {
+        "node_f1": node_f1,
+        "link_auc": link_auc,
+        "final_score": final_score,
+    }
+    
+    with open("scores.json", "w") as f:
+        json.dump(scores, f)
 
 if __name__ == "__main__":
     main()
